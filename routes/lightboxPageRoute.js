c663d35736af26e0e25f1a9c97a3838e2ad8af9d
rauuuf <lieberg@gmail.com>
2023-05-28 07:20:48 +0200

    ready deploy to test

--- routes/lightboxPageRoute.js
+++ routes/lightboxPageRoute.js
@@ -3,16 +3,17 @@ const router = express.Router();
 const { getAllDbAlbums, getImagesInAlbum, getAlbumName } = require('../utils/db/db-crud.js');
 
 // Image Gallery Page Route
-router.get('/:albumName', async (req, res) => {
+router.get('/:albumName/:albumId', async (req, res) => {
   console.log('In lightboxPageRoute.js');
   const albumName = req.params.albumName;
+  const albumId = req.params.albumId;
   // for the navbar albums list
   const allAlbums = await getAllDbAlbums();
   console.log('in image gallery page route');
 
 console.log('albumName: ', albumName);
 
-res.render('lightbox-page', { allAlbums, albumName });
+res.render('lightbox-page', { allAlbums, albumName, albumId });
 });
 
 
